687b86e0c2af5eca9e835b8dad2b8c6934bb8875
Eslaro <semoralesm@unal.edu.co>
2024-12-13 02:45:13 -0500

    xd

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -4,37 +4,18 @@ import java.util.ArrayList;
 public class Fabricante {
     private String nombre;
     private Pais pais;
-
     private static ArrayList<Fabricante> fabricantes = new ArrayList<>();
     private int cantidadVehiculosVendidos;
 
     public Fabricante(String nombre, Pais pais) {
         this.nombre = nombre;
         this.pais = pais;
-        this.cantidadVehiculosVendidos = 0; 
-        fabricantes.add(this); 
+        this.cantidadVehiculosVendidos = 0;
+        fabricantes.add(this);
     }
 
     public void incrementarVentas() {
         this.cantidadVehiculosVendidos++;
-    }
-
-    public static Fabricante fabricaMayorVentas() {
-        if (fabricantes.isEmpty()) {
-            return null; 
-        }
-
-        Fabricante fabricaMayorVentas = null;
-        int maxVentas = 0;
-
-        for (Fabricante fabricante : fabricantes) {
-            if (fabricante.getCantidadVehiculosVendidos() > maxVentas) {
-                maxVentas = fabricante.getCantidadVehiculosVendidos();
-                fabricaMayorVentas = fabricante;
-            }
-        }
-
-        return fabricaMayorVentas;
     }
 
     public static ArrayList<Fabricante> getFabricantes() {
@@ -51,5 +32,21 @@ public class Fabricante {
 
     public int getCantidadVehiculosVendidos() {
         return cantidadVehiculosVendidos;
+    }
+
+    // Método para encontrar la fábrica con más ventas
+    public static Fabricante fabricaMayorVentas() {
+        int maxVentas = 0;
+        Fabricante fabricaMasVendedora = null;
+
+        // Iterar por cada fabricante
+        for (Fabricante fabricante : fabricantes) {
+            if (fabricante.getCantidadVehiculosVendidos() > maxVentas) {
+                maxVentas = fabricante.getCantidadVehiculosVendidos();
+                fabricaMasVendedora = fabricante;
+            }
+        }
+
+        return fabricaMasVendedora;
     }
 }
--- src/test/java/vehiculos/Pais.java
+++ src/test/java/vehiculos/Pais.java
@@ -3,41 +3,36 @@ import java.util.ArrayList;
 
 public class Pais {
     private String nombre;
-
     private static ArrayList<Pais> paises = new ArrayList<>();
 
     public Pais(String nombre) {
         this.nombre = nombre;
-        paises.add(this); 
-    }
-
-    public static Pais paisMasVendedor() {
-        if (paises.isEmpty()) {
-            return null; 
-        }
-
-        Pais paisMasVendedor = null;
-        int maxVentas = 0;
-
-        for (Pais pais : paises) {
-            int ventasPorPais = 0;
-
-            for (Fabricante fabricante : Fabricante.getFabricantes()) {
-                if (fabricante.getPais().equals(pais)) {
-                    ventasPorPais += fabricante.getCantidadVehiculosVendidos();
-                }
-            }
-
-            if (ventasPorPais > maxVentas) {
-                maxVentas = ventasPorPais;
-                paisMasVendedor = pais;
-            }
-        }
-
-        return paisMasVendedor;
+        paises.add(this);
     }
 
     public String getNombre() {
         return nombre;
+    }
+
+    public static Pais paisMasVendedor() {
+        int maxVentas = 0;
+        Pais paisMasVendido = null;
+
+        for (Pais pais : paises) {
+            int ventasPais = 0;
+
+            for (Fabricante fabricante : Fabricante.getFabricantes()) {
+                if (fabricante.getPais() == pais) {
+                    ventasPais += fabricante.getCantidadVehiculosVendidos();
+                }
+            }
+
+            if (ventasPais > maxVentas) {
+                maxVentas = ventasPais;
+                paisMasVendido = pais;
+            }
+        }
+
+        return paisMasVendido;
     }
 }
